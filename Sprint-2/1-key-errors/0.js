a564a040eadf74a9ada077f62c2096ffb10c8fe2
joanne342 <130585013+joanne342@users.noreply.github.com>
2026-06-16 15:20:14 +0100

    Update 0.js

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -10,4 +10,14 @@ function capitalise(str) {
 }
 
 // =============> write your explanation here
+
+// Your need to delete "let ".  That's just for making a new variables.  You want assignment.
+
 // =============> write your new code here
+
+function capitalise(str) {
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str;
+}
+
+console.log(capitalise("hello"));
