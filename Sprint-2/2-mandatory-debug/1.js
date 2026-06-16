2ac82b26b47dd808933692d944ff8afe31917917
joanne342 <130585013+joanne342@users.noreply.github.com>
2026-06-16 15:21:41 +0100

    Update 1.js

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,6 +1,8 @@
 // Predict and explain first...
 //  =============> write your prediction here
 
+// It returns blank since it doesn't know what it's returning
+
 function sum(a, b) {
   return;
   a + b;
@@ -9,5 +11,14 @@ function sum(a, b) {
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
+
+  return a + b is supposed to be on one line.  "return;" just returns nothing
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
