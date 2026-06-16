dc6860811a06407fbfe8c148ae96162c23373a9f
joanne342 <130585013+joanne342@users.noreply.github.com>
2026-06-16 15:23:28 +0100

    Update 3-to-pounds.js

--- Sprint-2/3-mandatory-implement/3-to-pounds.js
+++ Sprint-2/3-mandatory-implement/3-to-pounds.js
@@ -4,3 +4,33 @@
 // You will need to declare a function called toPounds with an appropriately named parameter.
 
 // You should call this function a number of times to check it works for different inputs
+
+function calculateBMI(penceString) {  
+
+  // remove last character
+  const penceStringWithoutTrailingP = penceString.substring(
+    0,
+    penceString.length - 1
+  );
+
+  // pad to the length of three
+  const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+
+  // remove the last two characters
+  const pounds = paddedPenceNumberString.substring(
+    0,
+    paddedPenceNumberString.length - 2
+  );
+
+  // get last two characters and pad to two if needed
+  const pence = paddedPenceNumberString
+    .substring(paddedPenceNumberString.length - 2)
+    .padEnd(2, "0");
+
+  console.log(`£${pounds}.${pence}`);
+  
+}
+
+console.log(calculateBMI("456p"))
+console.log(calculateBMI("545747p"))
+console.log(calculateBMI("1p"))
