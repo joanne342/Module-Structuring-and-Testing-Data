a73a6f81981a7535341e48c1e33b7fd8ae125408
joanne342 <130585013+joanne342@users.noreply.github.com>
2026-06-16 15:13:43 +0100

    Update 4-random.js

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -7,3 +7,10 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+// get the maximum minus minimum
+// add 1 to count the edges and avoid the fencepost error
+// this generates the correct span
+// add the minimum on so it starts at the right number
+// multiply it by a random decimal between 0 and 1 
+// round it down to the next whole number down
