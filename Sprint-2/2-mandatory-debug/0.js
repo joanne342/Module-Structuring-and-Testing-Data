4b86b0113778694a7478ef6440ccb7190260335f
joanne342 <130585013+joanne342@users.noreply.github.com>
2026-06-16 15:21:20 +0100

    Update 0.js

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -2,6 +2,8 @@
 
 // =============> write your prediction here
 
+// The result of multiplying 10 and 32 is undefined
+
 function multiply(a, b) {
   console.log(a * b);
 }
@@ -10,5 +12,13 @@ console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // =============> write your explanation here
 
+// Nothing is returned in the function but rather logged to the console
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
