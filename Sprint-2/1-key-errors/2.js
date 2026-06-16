e3cf321f6a5c6230979198b078a7b5f179040506
joanne342 <130585013+joanne342@users.noreply.github.com>
2026-06-16 15:20:52 +0100

    Update 2.js

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -5,16 +5,26 @@
 
 // =============> write your prediction of the error here
 
+// num is not defined
+
 function square(3) {
     return num * num;
 }
 
 // =============> write the error message here
 
+// SyntaxError: Unexpected token (1:16)
+
 // =============> explain this error message here
+
+// You can't feed the value in there.  You need to call it from outside the function.  JS thinks it's a variable name which isn't allowed to start with a number.
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
 
+function square(num) {
+    return num * num;
+}
 
+console.log(square(2))
