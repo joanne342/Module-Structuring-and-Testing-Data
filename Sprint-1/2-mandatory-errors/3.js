07ec3e9d1e32e76c0b44bee2a807c2740708738d
joanne342 <130585013+joanne342@users.noreply.github.com>
2026-06-16 15:16:15 +0100

    Update 3.js

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,5 +1,6 @@
-const cardNumber = 4533787178994213;
-const last4Digits = cardNumber.slice(-4);
+const cardNumber = "4533787178994213";
+const last4Digits = cardNumber.slice(cardNumber.length-4,cardNumber.length);
+console.log(last4Digits)
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
