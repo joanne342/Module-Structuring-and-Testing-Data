cd63993d06170791876f373ebb87f54f098ae1ba
joanne342 <130585013+joanne342@users.noreply.github.com>
2026-06-16 15:23:53 +0100

    Update 2-cases.js

--- Sprint-2/3-mandatory-implement/2-cases.js
+++ Sprint-2/3-mandatory-implement/2-cases.js
@@ -14,3 +14,9 @@
 // You will need to come up with an appropriate name for the function
 // Use the MDN string documentation to help you find a solution
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+
+function snakeCase(toConvert) {
+  return toConvert.replace(" ", "_").toUpperCase();
+}
+
+console.log(snakeCase("hello there"));
