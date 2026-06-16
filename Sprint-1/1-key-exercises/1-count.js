d496fae6ff0b32d90c14cd524083cba332be8532
joanne342 <130585013+joanne342@users.noreply.github.com>
2026-06-16 15:11:55 +0100

    Update 1-count.js

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -2,5 +2,9 @@ let count = 0;
 
 count = count + 1;
 
+// The single equals means assignment. 
+// It is saying the value of count is now the previous value of count plus one.  
+//If it was == or ===, it would be testing equality and asking if count (0) is equal to 0+1 which would give false.
+
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
