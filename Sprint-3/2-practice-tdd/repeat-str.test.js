0ff0585c5db1dba52fa6353f6e445b3997524b0a
joanne342 <130585013+joanne342@users.noreply.github.com>
2026-06-17 09:33:37 +0100

    Update repeat-str.test.js

--- Sprint-3/2-practice-tdd/repeat-str.test.js
+++ Sprint-3/2-practice-tdd/repeat-str.test.js
@@ -21,12 +21,35 @@ test("should repeat the string count times", () => {
 // When the repeatStr function is called with these inputs,
 // Then it should return the original `str` without repetition.
 
+test("should return the original string when count is 1", () => {
+  const str = "hello";
+  const count = 1;
+  const repeatedStr = repeatStr(str, count);
+  expect(repeatedStr).toEqual("hello");
+});
+
 // Case: Handle count of 0:
 // Given a target string `str` and a `count` equal to 0,
 // When the repeatStr function is called with these inputs,
 // Then it should return an empty string.
 
+test("should return an empty string when count is 0", () => {
+  const str = "hello";
+  const count = 0;
+  const repeatedStr = repeatStr(str, count);
+  expect(repeatedStr).toEqual("");
+});
+
 // Case: Handle negative count:
 // Given a target string `str` and a negative integer `count`,
 // When the repeatStr function is called with these inputs,
 // Then it should throw an error, as negative counts are not valid.
+
+test("should throw an error when count is negative", () => {
+  const str = "hello";
+  const count = -2;
+
+  expect(() => {
+    repeatStr(str, count);
+  }).toThrow();
+});
