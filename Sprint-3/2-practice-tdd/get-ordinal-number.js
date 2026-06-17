2bb087aa911813c79c72e7b8afb56005f5807bd1
joanne342 <130585013+joanne342@users.noreply.github.com>
2026-06-17 09:33:27 +0100

    Update get-ordinal-number.js

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,23 @@
 function getOrdinalNumber(num) {
-  return "1st";
+    const asString = String(num);
+
+    if (num === 0) {
+        return "0";
+    } else if (
+        asString.endsWith("11") ||
+        asString.endsWith("12") ||
+        asString.endsWith("13")
+    ) {
+        return asString + "th";
+    } else if (asString.endsWith("1")) {
+        return asString + "st";
+    } else if (asString.endsWith("2")) {
+        return asString + "nd";
+    } else if (asString.endsWith("3")) {
+        return asString + "rd";
+    } else {
+        return asString + "th";
+    }
 }
 
 module.exports = getOrdinalNumber;
