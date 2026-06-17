191c3147e0b56837ccfd8d997f31488094ccfec1
joanne342 <130585013+joanne342@users.noreply.github.com>
2026-06-17 09:33:13 +0100

    Update 2-is-proper-fraction.test.js

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -8,3 +8,32 @@ const isProperFraction = require("../implement/2-is-proper-fraction");
 test(`should return false when denominator is zero`, () => {
   expect(isProperFraction(1, 0)).toEqual(false);
 });
+
+test("1/2 is proper", () => {
+  expect(isProperFraction(1, 2)).toBe(true);
+});
+
+test("3/2 is improper", () => {
+  expect(isProperFraction(3, 2)).toBe(false);
+});
+
+test("0/5 is proper", () => {
+  expect(isProperFraction(0, 5)).toBe(true);
+});
+
+test("denominator 0 is invalid", () => {
+  expect(isProperFraction(1, 0)).toBe(false);
+});
+
+test("-1/2 is proper", () => {
+  expect(isProperFraction(-1, 2)).toBe(true);
+});
+
+test("1/-2 is proper", () => {
+  expect(isProperFraction(1, -2)).toBe(true);
+});
+
+test("-3/2 is improper", () => {
+  expect(isProperFraction(-3, 2)).toBe(false);
+});
+
