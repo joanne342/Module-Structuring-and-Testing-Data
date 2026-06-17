fee0a071bb9f690b4313b5a1b964218e01521c9a
joanne342 <130585013+joanne342@users.noreply.github.com>
2026-06-17 09:33:30 +0100

    Update get-ordinal-number.test.js

--- Sprint-3/2-practice-tdd/get-ordinal-number.test.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.test.js
@@ -18,3 +18,40 @@ test("should append 'st' for numbers ending with 1, except those ending with 11"
   expect(getOrdinalNumber(21)).toEqual("21st");
   expect(getOrdinalNumber(131)).toEqual("131st");
 });
+
+/// Case 2: Numbers ending with 2 (but not 12)
+test("should append 'nd' for numbers ending with 2, except those ending with 12", () => {
+  expect(getOrdinalNumber(2)).toEqual("2nd");
+  expect(getOrdinalNumber(22)).toEqual("22nd");
+  expect(getOrdinalNumber(102)).toEqual("102nd");
+});
+
+// Case 3: Numbers ending with 3 (but not 13)
+test("should append 'rd' for numbers ending with 3, except those ending with 13", () => {
+  expect(getOrdinalNumber(3)).toEqual("3rd");
+  expect(getOrdinalNumber(23)).toEqual("23rd");
+  expect(getOrdinalNumber(103)).toEqual("103rd");
+});
+
+// Case 4: Numbers ending with 11, 12, or 13 should always use 'th'
+test("should append 'th' for numbers ending with 11, 12, or 13", () => {
+  expect(getOrdinalNumber(11)).toEqual("11th");
+  expect(getOrdinalNumber(12)).toEqual("12th");
+  expect(getOrdinalNumber(13)).toEqual("13th");
+  expect(getOrdinalNumber(111)).toEqual("111th");
+  expect(getOrdinalNumber(212)).toEqual("212th");
+  expect(getOrdinalNumber(313)).toEqual("313th");
+});
+
+// Case 5: All other numbers should use 'th'
+test("should append 'th' for numbers that do not end in 1, 2, or 3", () => {
+  expect(getOrdinalNumber(4)).toEqual("4th");
+  expect(getOrdinalNumber(10)).toEqual("10th");
+  expect(getOrdinalNumber(20)).toEqual("20th");
+  expect(getOrdinalNumber(99)).toEqual("99th");
+});
+
+// Case 6: Zero should return '0'
+test("should return '0' when the input is zero", () => {
+  expect(getOrdinalNumber(0)).toEqual("0");
+});
