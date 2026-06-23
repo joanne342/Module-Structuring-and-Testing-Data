da301ecee55f41c62dffc883ad2e2c0b60d9d200
joanne342 <130585013+joanne342@users.noreply.github.com>
2026-06-23 08:41:25 +0100

    Update repeat-str.js

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,7 +1,7 @@
 function repeatStr() {
   // Your implementation of this function must *not* call String.prototype.repeat (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat).
   // The goal is to re-implement that function, not to use it.
-  return str.repeat(count);
+  return Array.from({ length: count }, () => str).join("");
 }
 
 module.exports = repeatStr;
