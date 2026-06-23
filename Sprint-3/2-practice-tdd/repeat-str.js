00c17a1500ecbd996d25368305080df36061e78d
joanne342 <130585013+joanne342@users.noreply.github.com>
2026-06-23 12:49:47 +0100

    Update repeat-str.js

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -2,7 +2,7 @@ function repeatStr(str, count) {
   // Your implementation of this function must *not* call String.prototype.repeat (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat).
   // The goal is to re-implement that function, not to use it.
 if (count < 0) {
-    throw new Error("Something went wrong");
+    throw new Error("Negative count is not allowed");
 
   } else if (count === 0) {
     return "";
